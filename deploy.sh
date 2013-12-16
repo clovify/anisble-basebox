f73c42240a8685d10c764df0e9ec67b1dbc3a581
Barney Hanlon <shrikeh@case.local>
2013-12-16 10:54:39 +0000

    t

--- deploy.sh
+++ deploy.sh
@@ -3,10 +3,9 @@
 set -e
 
 export HOSTFILE='/Users/shrikeh/Projects/shrikeh.net/anisble-basebox/hosts/production'
-export PLAYBOOK_PATH='/Users/shrikeh/Projects/shrikeh.net/anisble-basebox';
-export PLAYBOOK_FILE="${PLAYBOOK_PATH}/base.yml";
+export PLAYBOOK_PATH='/Users/shrikeh/Projects/shrikeh.net/ansible';
+export PLAYBOOK_FILE="${PLAYBOOK_PATH}/playbook.yml";
 
 export ANSIBLE_HOST_KEY_CHECKING=false
-export EXTRA_VARS_FILE="${HOME}/test.json"
 
-ansible-playbook -vvv ${PLAYBOOK_FILE} -v -i "${HOSTFILE}" --extra-vars "@${EXTRA_VARS_FILE}";
+ansible-playbook -vvv ${PLAYBOOK_FILE} -vvv -i "${HOSTFILE}";
